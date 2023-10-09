d99855ef265aaff00245cbf33e5747a973d99242
dharmiksoni <dharmiksoni1996@gmail.com>
2023-10-09 19:27:30 +0530

    extra information

---
+++ tuning.sql
@@ -0,0 +1,18 @@
+/*
+Tuning in mssql refers to process of optimizing the performance of your database system by adjusting various
+parameters, configurations and structures to ensure efficient query execution and resource utilization.
+
+Purpose
+1. Performance improvements
+2. Effecient resource utilization
+3. Optimized query execution
+
+Methods
+1. Index optimization
+2. Query optimization
+3. Database design - good database design with minimum redudancy and high data integrity
+4. Hardware scaling
+5. Monitoring and profiling
+6. Query execution plans
+
+*/
